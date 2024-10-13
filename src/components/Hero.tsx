3c4ccfe542d0011a500b03ca1fa6a99cee6d967c
Pizza6inch <pizza6inch@gmail.com>
2024-10-13 19:49:30 +0800

    搞定動畫

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,44 +3,142 @@ import ribbon2 from "../assets/ribbons2.svg";
 import firecracker1 from "../assets/firecracker1.svg";
 import firecracker2 from "../assets/firecracker2.svg";
 import cake from "../assets/cake.svg";
+import { color, motion, spring } from "framer-motion";
+import { text } from "framer-motion/client";
+
+const Congratulations = [
+  { text: "風のように自由で、", color: "text-green-400" },
+  { text: "花のように美しい", color: "text-red-400" },
+  { text: "日々を過ごせますように。", color: "black" },
+];
+
+const LetterWithAnimate: React.FC = () => (
+  <>
+    {Congratulations.map(({ text, color }, lineIndex) => (
+      <motion.span
+        key={lineIndex}
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, delay: lineIndex * 0.5 }} // 每行延迟 1 秒
+      >
+        {text.split("").map((letter, index) => (
+          <motion.span
+            key={index}
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{
+              duration: 0.2,
+              delay: lineIndex * 0.5 + index * 0.05,
+            }} // 每个字母延迟 0.05 秒
+            className={`font-bold ${color}`}
+          >
+            {letter}
+          </motion.span>
+        ))}
+      </motion.span>
+    ))}
+  </>
+);
+
 const Hero = () => {
   return (
     <div className="bg-[#F7E4DF] w-full h-[630px] flex flex-col justify-between relative">
-      <img
-        src={ribbon1}
-        alt="ribbon"
-        className="absolute flex left-0 top-0 w-1/3"
-      />
-      <img
-        src={ribbon2}
-        alt="ribbon"
-        className="absolute flex right-0 top-0 w-1/3 "
-      />
-      <div className=" w-full h-full pt-8 flex flex-col items-center">
-        <h2 className="">嘿！</h2>
-        <h1 className="">老姊</h1>
-        <p className="">生日快樂啦！</p>
-        <p className=" font-jpFont text-4xl">
-          風のように自由で、花のように美しい日々を過ごせますように。
+      <motion.div
+        initial={{ y: 100, opacity: 0 }} // 初始状态
+        whileInView={{ y: 0, opacity: 1 }} // 在视口内时的状态
+        viewport={{ once: true }} // 只在第一次进入视口时触发
+        transition={{ duration: 2 }}
+      >
+        <img
+          src={ribbon1}
+          alt="ribbon"
+          className="absolute flex left-0 top-0 w-1/3"
+        />
+        <img
+          src={ribbon2}
+          alt="ribbon"
+          className="absolute flex right-0 top-0 w-1/3 "
+        />
+      </motion.div>
+      <div className=" w-full h-full pt-12 flex flex-col items-center">
+        {/* <h2 className=" text-gray-800 font-semibold text-[28px]">嘿！</h2> */}
+        <h1 className="text-black font-bold text-[40px]">To 姵涵姊姊</h1>
+        <p className="text-[#103362] font-enFont mt-10">
+          {"Happy Birthday！".split("").map((letter, index) => (
+            <motion.span
+              key={index}
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{
+                duration: 0.2,
+                delay: index * 0.05,
+              }} // 每个字母延迟 0.05 秒
+              className={`text-[80px] font-bold`}
+            >
+              {letter}
+            </motion.span>
+          ))}
         </p>
-        <h2> from 匯吾</h2>
+        <p className=" font-jpFont text-4xl mt-4">
+          <LetterWithAnimate />
+        </p>
+        <motion.h2
+          className="text-gray-800 font-semibold text-[28px] mt-10"
+          initial={{ rotate: 270, opacity: 0 }} // 初始状态
+          whileInView={{ rotate: 0, opacity: 1 }} // 在视口内时的状态
+          viewport={{ once: true }} // 只在第一次进入视口时触发
+          transition={{ duration: 0.5, delay: 1.5 }}
+        >
+          from 匯吾
+        </motion.h2>
       </div>
-
-      <img
-        src={firecracker1}
-        alt="firecracker"
-        className=" absolute left-0 bottom-0"
-      />
-      <img
-        src={cake}
-        alt="cake"
-        className=" absolute left-1/2 bottom-0 transform -translate-x-1/2"
-      />
-      <img
-        src={firecracker2}
-        alt="firecracker"
-        className=" absolute right-0 bottom-0"
-      />
+      <motion.div
+        initial={{ scale: 2, opacity: 0 }} // 初始状态
+        whileInView={{ scale: 1, opacity: 1 }} // 在视口内时的状态
+        viewport={{ once: true }} // 只在第一次进入视口时触发
+        transition={{
+          type: "spring", // 使用 spring 动画类型
+          stiffness: 100, // 弹簧的刚度
+          damping: 5, // 阻尼
+          duration: 2, // 动画持续时间
+        }}
+      >
+        <img
+          src={firecracker1}
+          alt="firecracker"
+          className=" absolute left-0 bottom-0"
+        />
+        <img
+          src={firecracker2}
+          alt="firecracker"
+          className=" absolute right-0 bottom-0"
+        />
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0 }} // 初始状态
+        whileInView={{ opacity: 1 }} // 在视口内时的状态
+        viewport={{ once: true }} // 只在第一次进入视口时触发
+        transition={{ duration: 2 }}
+      >
+        <motion.div
+          initial={{ scale: 0.01, opacity: 0 }} // 初始状态
+          whileInView={{ scale: 1, opacity: 1 }} // 在视口内时的状态
+          viewport={{ once: true }} // 只在第一次进入视口时触发
+          transition={{
+            type: spring,
+            stiffness: 100, // 弹簧的刚度
+            damping: 5,
+            duration: 1,
+            delay: 2.5,
+          }}
+        >
+          <img
+            src={cake}
+            alt="cake"
+            className=" absolute left-1/2 bottom-0 transform -translate-x-1/2"
+          />
+        </motion.div>
+      </motion.div>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ module.exports = {
     extend: {
       fontFamily: {
         jpFont: ["KTEGAKI", "sans-serif"],
+        enFont: ["Oldenburg", "sans-serif"],
       },
     },
   },
