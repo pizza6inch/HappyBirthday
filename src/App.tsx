148368f5566de68a7b1e7142ba2dad93e65a48ca
Pizza6inch <pizza6inch@gmail.com>
2024-10-13 14:47:10 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="bg-[#F7E4DF]">
+      <h1 className="text-4xl font-bold text-blue-500">
+        Hello, Vite + React + TypeScript + Tailwind!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
