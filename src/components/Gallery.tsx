026848cf4981c7515d42175f095216e2c6a11db1
huiwuzhang <huiwuzhang@eztravel.com.tw>
2024-10-14 14:01:43 +0800

    完成rwd的畫廊

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,7 +1,13 @@
 import { motion } from "framer-motion";
 import useDeviceType from "../hooks/useDeviceType";
 
+import { Swiper, SwiperSlide } from "swiper/react";
+// import { Autoplay } from "swiper";
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
+
+import "swiper/swiper-bundle.css"; // 适用于 Swiper v9 和更高版本
 import { Tilt } from "react-tilt";
+
 import photo1 from "/public/photos/photo1.jpg";
 import photo2 from "/public/photos/photo2.jpg";
 import photo3 from "/public/photos/photo3.jpg";
@@ -28,6 +34,7 @@ const Photo = ({ src, delay = 0 }: { src: string; delay?: number }) => {
 };
 
 const Gallery = () => {
+  const device = useDeviceType();
   return (
     <div className="bg-gradient-to-br from-[#F7E4DF] to-[#F0C2B2] pt-4 px-8 flex flex-col items-center relative">
       {/* 裝飾元素 */}
@@ -47,12 +54,46 @@ const Gallery = () => {
       >
         Memories
       </motion.h2>
-      <div className=" grid grid-cols-2 my-16 gap-40 ">
-        <Photo src={photo3} />
-        <Photo src={photo1} delay={0.5} />
-        <Photo src={photo2} />
-        <Photo src={photo4} delay={0.5} />
-      </div>
+      {device === "desktop" && (
+        <div className=" grid grid-cols-2 my-16 gap-40 ">
+          <Photo src={photo3} />
+          <Photo src={photo1} delay={0.5} />
+          <Photo src={photo2} />
+          <Photo src={photo4} delay={0.5} />
+        </div>
+      )}
+      {device === "mobile" && (
+        <div className=" relative w-full">
+          <div className="absolute top-8 left-1/2 transform -translate-x-1/2 animate-bounce-horizontal">
+            <svg className="h-6 w-6 text-gray-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 15h20m0 0l-5-5m5 5l-5 5" />
+            </svg>
+          </div>
+          <Swiper
+            modules={[Pagination, Scrollbar, A11y]}
+            pagination={true}
+            autoplay={{ delay: 3000 }}
+            className="my-16 w-full rounded-lg relative"
+            spaceBetween={0}
+            slidesPerView={1}
+            onSwiper={(swiper) => console.log(swiper)}
+            onSlideChange={() => console.log("slide change")}
+          >
+            <SwiperSlide>
+              <img src={photo3} alt="photo1" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src={photo1} alt="photo2" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src={photo2} alt="photo3" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src={photo4} alt="photo4" />
+            </SwiperSlide>
+          </Swiper>
+        </div>
+      )}
     </div>
   );
 };
